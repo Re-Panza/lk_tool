1c50cab0228697e170362077d060313a1cbdfa15
Re-Panza <111627325+Re-Panza@users.noreply.github.com>
2026-01-26 16:47:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2.3';
+const CACHE_VERSION = 'v2.4';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const PAGES_CACHE = `pages-${CACHE_VERSION}`;
 
@@ -69,6 +69,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
